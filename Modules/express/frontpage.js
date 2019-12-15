2c1573baf40901b1100821f4370b16b97eb0b4ff
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-12-15 17:29:34 +0200

    contined webserver

--- Modules/express/frontpage.js
+++ Modules/express/frontpage.js
@@ -12,4 +12,16 @@ router.get('/', function (req, res) {
   res.sendFile(path.resolve('./Modules/express/html/index.html'));
 });
 
+router.get('/:file', function (req, res) {
+  console.log(req.params.file);
+  var fileName = req.params.file;
+  if (!fileName.includes('/')) {
+    if (fileName.endsWith(".css")) {
+      
+    }if (fileName.endsWith(".js")) {
+      
+    }
+  }
+});
+
 module.exports = router;
